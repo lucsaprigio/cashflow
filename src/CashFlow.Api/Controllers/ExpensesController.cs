62e56c6986b6aa7ebfd93cdb875157946dea5a65
Lucas Aprigio <lucsaprigio@hotmail.com>
2026-01-03 08:25:12 -0300

    App

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -21,11 +22,15 @@ public class ExpensesController : ControllerBase
         }
         catch (ArgumentException ex)
         {
-            return BadRequest(ex.Message);
+            var errorResponse = new ResponseErrorJson(ex.Message);
+
+            return BadRequest(errorResponse);
         }
         catch
         {
-            return StatusCode(StatusCodes.Status500InternalServerError, "unknow error");
+            var errorResponse = new ResponseErrorJson("Unknown error while processing the request.");
+
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
     }
 }
